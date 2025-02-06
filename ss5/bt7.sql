91af327813f5f3b94562675947fffe72ea7e8e7d
thanhnambun <147359810+thanhnambun@users.noreply.github.com>
2025-02-06 10:53:12 +0700

    Add files via upload

--- ss5/bt7.sql
+++ ss5/bt7.sql
@@ -14,10 +14,14 @@ LEFT JOIN students s ON e.student_id = s.student_id
 WHERE c.fee > 1000000  OR e.course_id is null
 ORDER BY c.fee desc , c.course_name asc ;
 
+select 
+    s.name as student_name, 
+    s.email, 
+    c.course_name, 
+    e.enrollment_date
+from students s
+left join courses c on e.course_id = c.course_id
+left join enrollments e on s.student_id = e.student_id
+where s.email is null or c.fee > 1000000
+order by s.name asc, c.course_name asc;
 
-SELECT s.name AS student_name, s.email, c.course_name, e.enrollment_date
-FROM students s
-CROSS JOIN courses c
-LEFT JOIN enrollments e ON s.student_id = e.student_id AND c.course_id = e.course_id
-WHERE s.email IS NULL OR c.fee > 1000000
-ORDER BY s.name ASC, c.course_name ASC;
