28774101d9498b0e606735ed823631ca0114ba26
thanhnambun <147359810+thanhnambun@users.noreply.github.com>
2025-02-13 11:19:03 +0700

    Add files via upload

--- ss10/bt10.sql
+++ ss10/bt10.sql
@@ -1,25 +1,39 @@
-create view OfficialLanguageView  
-as select c.code, c.name ,cl.Language
-        from country c
-        join countrylanguage cl on c.Code = cl.CountryCode 
-        where cl.IsOfficial ='t' ;
-        
-select * from OfficialLanguageView;
+use world;
 
-create index spead_name on city(name);
+create view officiallanguageview as
+select 
+    country.code as countrycode, 
+    country.name as countryname, 
+    countrylanguage.language
+from country
+join countrylanguage on country.code = countrylanguage.countrycode
+where countrylanguage.isofficial = 'T';
 
-delimiter // 
-	create procedure GetSpecialCountriesAndCities  ( in language_name varchar(25))
-    begin
-		select c.name as CountryName  , ct.name as CityName , ct.population as CityPopulation  ,c.popuolation as TotalPopulation  
-        from country c 
-        join city ct on ct.countrycode = c.code
-        where language_name = ct.language and c.popuolation > 5000000 and ct.name like 'new%'
-        order by c.popuolation desc 
-        limit 10;
-        
-    end;
-// delimiter ;
+select * from officiallanguageview;
 
-call GetSpecialCountriesAndCities('english');
+create index idx_city_name on city(name);
 
+delimiter //
+create procedure GetSpecialCountriesAndCities(in language_name char(30))
+begin
+    select 
+        country.name as countryname, 
+        city.name as cityname, 
+        city.population as citypopulation,
+        (select sum(city.population) 
+         from city 
+         where city.countrycode = country.code) as totalpopulation
+    from city
+    join country on city.countrycode = country.code
+    join countrylanguage on country.code = countrylanguage.countrycode
+    where countrylanguage.language = language_name 
+        and countrylanguage.isofficial = 'T'
+        and city.name like 'New%'
+    group by country.name, city.name, city.population
+    having totalpopulation > 5000000
+    order by totalpopulation desc
+    limit 10;
+end 
+//delimiter ;
+
+call GetSpecialCountriesAndCities('English');
